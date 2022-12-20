972edc8180dbec4d5449e7d088697df9b4b65c60
cheol <qjel60@naver.com>
2022-12-20 16:28:07 +0900

    feat:boar d게시판 완료

--- routes/board.route.js
+++ routes/board.route.js
@@ -1,24 +1,48 @@
 const express = require("express");
 const route = express.Router();
+const list = [{ title: "첫번째 글", writer: "1등", content: "1등" }];
 
-route.get("list", (req, res) => {
-    res.send("board/ list 입니다");
+route.get("/list", (req, res) => {
+    res.render("board/list.html", { list });
 });
 
 route.get("/write", (req, res) => {
-    res.send("board/ write 입니다");
+    res.render("board/write.html");
+});
+
+route.post("/write", (req, res) => {
+    const { title, writer, content } = req.body;
+    list.push({ title, writer, content });
+    res.redirect(`view?index=${list.length - 1}`);
 });
 
 route.get("/modify", (req, res) => {
-    res.send("board/ modify");
+    const { index } = req.query;
+    const item = {
+        ...list[index],
+        index,
+    };
+    res.render("board/modify.html", { item });
+});
+
+route.post("/modify", (req, res) => {
+    const { index, ...items } = req.body;
+    list[index] = items;
+    res.redirect(`view?index=${index}`);
 });
 
 route.get("/delete", (req, res) => {
-    res.send("board/ delete");
+    const { index } = req.query;
+    list.splice(index, 1);
+    res.redirect("list");
 });
 
 route.get("/view", (req, res) => {
-    res.send("board/ view");
+    const { index } = req.query;
+    const item = {
+        ...list[index],
+        index,
+    };
+    res.render("board/view.html", { item });
 });
-
 module.exports = route;
