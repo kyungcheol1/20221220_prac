56aaaa80075081f64dcf61fe3aae9158dabf3ffd
cheol <qjel60@naver.com>
2022-12-20 14:34:12 +0900

    feat:server rlqhs tpxd

--- server.js
+++ server.js
@@ -1,2 +1,13 @@
 const express = require("express");
+const app = express();
 const route = require("./routes/index");
+const nunjucks = require("nunjucks");
+
+app.set("view engine", "html");
+app.use(express.static("public"));
+nunjucks.configure("views", { express: app });
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(3000, () => {
+    console.log("server start");
+});
